6f7aa66c41e34b2ce08f7cf9fe220a5ef26f754f
Valera <inifrey@gmail.com>
2017-03-14 21:53:26 +0200

    Объединение с локальной версией

---
+++ create.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Валерий
+ * Date: 08.03.2017
+ * Time: 23:44
+ */
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 /**
- * Created by Валерий on 05.03.2017.
+ * Created by Р’Р°Р»РµСЂРёР№ on 05.03.2017.
  */
+
+$( document ).ready(function() {
+
+    $('#firstPage').show();
+
+    $('.showPage').on('click', function(){
+        $('.switchPage').fadeOut(400);
+        var page = $(this).data('show');
+        $('#'+page).fadeIn(200);
+    });
+
+    $('#logo').on('click', function(){
+        location.reload();
+    });
+
+});
